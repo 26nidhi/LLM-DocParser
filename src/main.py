06000009bb1ea70c7254175b2ed7ff8bcc079b44
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-12-05 00:21:32 +0530

    main function created

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from extractor import extract_pdf_text
+from llm_processor import extract_key_values
+from excel_writer import write_to_excel
+
+def main():
+    pdf_path = "../Data Input.pdf"
+    output_path = "../Output.xlsx"
+
+    print("Extracting PDF...")
+    full_text = extract_pdf_text(pdf_path)
+
+    print("Sending full text to LLM (single call)...")
+    rows = extract_key_values(full_text)
+
+    print("Writing Excel...")
+    write_to_excel(rows, output_path)
+
+    print("DONE → Output.xlsx generated successfully.")
+
+if __name__ == "__main__":
+    main()
