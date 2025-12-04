85301eb3ffe037c21dea470cd96054962c6f8907
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-12-05 00:21:00 +0530

    extractor fuction created

---
+++ src/extractor.py
@@ -0,0 +1,10 @@
+import pdfplumber
+
+def extract_pdf_text(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            t = page.extract_text()
+            if t:
+                text += "\n" + t
+    return text.strip()
